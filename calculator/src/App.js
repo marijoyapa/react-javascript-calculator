9bda86fde807453a041c7f26a93658e2fe8e1492
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-09-08 09:11:48 +0800

    Create structure

---
+++ calculator/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.setState(
+
+    )
+  }
+
+  render(){
+    return(
+      <div>Hello world</div>
+    )
+  }
+
+
+
+}
+
+
+
+export default Calculator;
