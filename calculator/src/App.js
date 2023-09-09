451da8066433bf8e515c58cab18088d7c2528bd6
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-09-09 22:19:01 +0800

    Fully functional calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -7,23 +7,92 @@ class Calculator extends React.Component {
     super(props)
 
     this.state = {
-      expression: "",
-      answer: ""
+      input: "",
+      output: "0",
+      answer: "",
+      nextInput: false
     }
     this.storeExpression = this.storeExpression.bind(this);
-    this.clearExpression = this.clearExpression.bind(this)
+    this.clearExpression = this.clearExpression.bind(this);
+    this.calculate = this.calculate.bind(this);
 
   }
 
-  storeExpression(e){
+  storeExpression(e) {
+    var expression = this.state.input.concat(e.target.value)
+    var output = ""
+    var input = e.target.value;
+    var lastTwo = expression.slice(-2)
+    if (this.state.nextInput == false) {
+      //handle input
+      //handle multiple decimal points
+      if (expression.includes("..")) {
+        expression = expression.replace("..", '.')
+      }
+      //handle number starts with zero
+      else if (expression[0] == 0) {
+        expression = expression.replace(expression[0], '')
+      }
+      //handle digits starting with multiple zeros
+      else if (expression === "00") {
+        expression = "0"
+      }
+      else if (lastTwo == "+-" | lastTwo == "+*" | lastTwo == "+/" | lastTwo == "-+" | lastTwo == "-*" | lastTwo == "-/" | lastTwo == "*+" | lastTwo == "*/" | lastTwo == "/+" | lastTwo == "++" | lastTwo == "--" | lastTwo == "**" | lastTwo == "//") {
+        expression = expression.replace(lastTwo, lastTwo[1])
+      }
+
+      //handle output
+      //handle when an operation is clicked
+      if (input == "+" | input == "-" | input == "*" | input == "/") {
+        output = input
+      }
+      else {
+        output = this.state.output.concat(e.target.value)
+        //to handle for expresssions starting with 0 and operations
+        if (output[0] == 0 | output[0] == "+" | output[0] == "-" | output[0] == "*" | output[0] == "/") {
+          output = output.replace(output[0], '')
+        }
+      }
+
+    }
+
+    else {
+      output = "0"
+      expression = ""
+    }
+
     this.setState({
-      expression: this.state.expression.concat(e.target.value)
+      input: expression,
+      output,
+      nextInput: false
     })
   }
-  clearExpression(){
+
+  clearExpression() {
     this.setState({
-      expression: ''
+      input: '',
+      output: '0'
     })
+  }
+  calculate() {
+
+    var expression = this.state.input;
+    console.log(expression.length)
+    if (this.state  .nextInput==false) {
+
+      var lastchar = expression.slice(-1)
+      if (lastchar == "+" | lastchar == "-" | lastchar == "*" | lastchar == "/") {
+        expression = expression.replace(expression[expression.length - 1], '')
+      }
+      var answer = eval(expression);
+      var input = expression.concat("=", answer)
+      this.setState({
+        input,
+        output: answer,
+        nextInput: true
+      })
+    }
+
   }
 
   render() {
@@ -32,27 +101,27 @@ class Calculator extends React.Component {
       <div className="app" >
         <div className="container">
           <section className="result">
-            <div> number </div>
-            <div>{this.state.expression}</div>
+            <div className="output">{this.state.input}</div>
+            <div className="output" id="display">{this.state.output}</div>
           </section>
           <section className="digits">
-            <button className="button ac" onClick={this.clearExpression}>AC</button>
-            <button className="button operator" onClick={this.storeExpression} value="/">/</button>
-            <button className="button operator" onClick={this.storeExpression} value="*">x</button>
-            <button className="button number" onClick={this.storeExpression} value="7">7</button>
-            <button className="button number" onClick={this.storeExpression} value="8">8</button>
-            <button className="button number" onClick={this.storeExpression} value="9">9</button>
-            <button className="button operator" onClick={this.storeExpression} value="-">-</button>
-            <button className="button number" onClick={this.storeExpression} value="4">4</button>
-            <button className="button number" onClick={this.storeExpression} value="5">5</button>
-            <button className="button number" onClick={this.storeExpression} value="6">6</button>
-            <button className="button operator" onClick={this.storeExpression} value="+">+</button>
-            <button className="button number" onClick={this.storeExpression} value="1">1</button>
-            <button className="button number" onClick={this.storeExpression} value="2">2</button>
-            <button className="button number" onClick={this.storeExpression} value="3">3</button>
-            <button className="button equals" onClick={this.storeExpression} value="=">=</button>
-            <button className="button number ac" onClick={this.storeExpression} value="0">0</button>
-            <button className="button number" onClick={this.storeExpression} value=".">.</button>
+            <button className="button ac red" onClick={this.clearExpression} id="clear">AC</button>
+            <button className="button operator" onClick={this.storeExpression} value="/" id="divide">/</button>
+            <button className="button operator" onClick={this.storeExpression} value="*" id="multiply">x</button>
+            <button className="button number" onClick={this.storeExpression} value="7" id="seven">7</button>
+            <button className="button number" onClick={this.storeExpression} value="8" id="eight">8</button>
+            <button className="button number" onClick={this.storeExpression} value="9" id="nine">9</button>
+            <button className="button operator" onClick={this.storeExpression} value="-" id="substract">-</button>
+            <button className="button number" onClick={this.storeExpression} value="4" id="four">4</button>
+            <button className="button number" onClick={this.storeExpression} value="5" id="five">5</button>
+            <button className="button number" onClick={this.storeExpression} value="6" id="six">6</button>
+            <button className="button operator" onClick={this.storeExpression} value="+" id="plus">+</button>
+            <button className="button number" onClick={this.storeExpression} value="1" id="one">1</button>
+            <button className="button number" onClick={this.storeExpression} value="2" id="two">2</button>
+            <button className="button number" onClick={this.storeExpression} value="3" id="three">3</button>
+            <button className="button equals" onClick={this.calculate} value="=" id="equals">=</button>
+            <button className="button number ac" onClick={this.storeExpression} value="0" id="zero">0</button>
+            <button className="button number" onClick={this.storeExpression} value="." id="decimal">.</button>
           </section>
         </div>
       </div>
